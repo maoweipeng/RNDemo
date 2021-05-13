cb9bfb919aa9529a3b3efcd63f451c09fef6fc61
Mao <Mao@fts.dev>
2021-05-13 18:34:31 +0800

    test

--- app/views/lottie/index.js
+++ app/views/lottie/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useCallback, useState, useEffect} from 'react'
 import {
   StatusBar,
   StyleSheet,
   useColorScheme,
   View,
+  Text,
   ScrollView,
 } from 'react-native'
 import {useFocusEffect} from '@react-navigation/native'
@@ -12,17 +13,39 @@ import Header from '../../components/header'
 import Menu from '../../components/lotties/menu'
 import Loading from '../../components/lotties/loading'
 import Smile from '../../components/lotties/smile'
+import Heart from '../../components/lotties/heart'
+import RefreshControl from '../../components/refreshControl'
 
 export default function Lottie({navigation}) {
   const isDarkMode = useColorScheme() === 'dark'
+  const [refreshing, setRefreshing] = useState(true)
+
+  useEffect(() => {
+    setRefreshing(true)
+
+    const timer = setTimeout(() => {
+      setRefreshing(false)
+    }, 5000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   useFocusEffect(() => {
     StatusBar.setBarStyle(isDarkMode ? 'dark-content' : 'light-content')
   })
 
-  function handlePress() {
+  const handlePress = useCallback(() => {
     navigation.pop()
-  }
+  }, [navigation])
+
+  const handleRefresh = useCallback(() => {
+    setRefreshing(true)
+    setTimeout(() => {
+      setRefreshing(false)
+    }, 5000)
+    console.log('onRefresh')
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -33,10 +56,44 @@ export default function Lottie({navigation}) {
         onLeftPress={handlePress}
       />
 
-      <ScrollView contentContainerStyle={styles.content}>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.content}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+            renderContent={() => <Text>refreshing</Text>}
+          />
+        }>
         <Menu />
         <Loading />
         <Smile />
+        <Heart />
+        <Menu />
+        <Loading />
+        <Smile />
+        <Heart />
+        <Menu />
+        <Loading />
+        <Smile />
+        <Heart />
+        <Menu />
+        <Loading />
+        <Smile />
+        <Heart />
+        <Menu />
+        <Loading />
+        <Smile />
+        <Heart />
+        <Menu />
+        <Loading />
+        <Smile />
+        <Heart />
+        <Menu />
+        <Loading />
+        <Smile />
+        <Heart />
       </ScrollView>
     </View>
   )
@@ -51,8 +108,10 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderBottomColor: '#eee',
   },
-  content: {
+  scrollView: {
     flex: 1,
+  },
+  content: {
     alignItems: 'center',
   },
 })
